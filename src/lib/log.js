bcdfaaae00c34e9ead173422fefda2478e4d36ce
LenAnderson <Anderson.Len@outlook.com>
2024-01-20 18:27:31 +0000

    fix log verbose check

--- src/lib/log.js
+++ src/lib/log.js
@@ -1,4 +1,4 @@
 import { extension_settings } from '../../../../../extensions.js';
 
-export const log = (...msg)=>(extension_settings.codex.isVerbose ?? true) && console.log('[STCDX2]', ...msg);
-export const warn = (...msg)=>(extension_settings.codex.isVerbose ?? true) && console.warn('[STCDX2]', ...msg);
+export const log = (...msg)=>(extension_settings.codex?.isVerbose ?? true) && console.log('[STCDX2]', ...msg);
+export const warn = (...msg)=>(extension_settings.codex?.isVerbose ?? true) && console.warn('[STCDX2]', ...msg);
